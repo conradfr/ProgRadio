3a37394f23c0e8ce8d4f45c1114fec1ac80a9dbc
Lionel Cocatrix <yoyolebatteur@gmail.com>
2017-08-10 13:23:51 +0200

    Add sitemap

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,11 +6,20 @@ use AppBundle\Service\ScheduleManager;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route(
+     *     "/",
+     *     name="homepage",
+     *     defaults={
+     *      "priority": "1.0",
+     *      "changefreq": "daily"
+     *      }
+     * )
      */
     public function indexAction(Request $request)
     {
@@ -25,5 +34,52 @@ class DefaultController extends Controller
             'schedule' => $schedule,
             'radios' => $radios
         ]);
+    }
+
+    /**
+     * Simple sitemap generator
+     *
+     * Doesn't use the serializer.
+     * While it would have looked cleaner it seems too much work to override full xml output.
+     *
+     * @Route(
+     *     "/sitemap.{_format}",
+     *     defaults={"_format": "xml"},
+     *     requirements={
+     *         "_format": "xml"
+     *     }
+     * )
+     */
+    public function sitemapAction(Request $request)
+    {
+        // @todo if route list grows, get collection & filter
+        $routesToExport = ['homepage'];
+        $routes = [];
+        foreach ($routesToExport as $entry) {
+            $routes[$entry] = $this->get('router')->getRouteCollection()->get($entry);
+        }
+
+        $xml = '<?xml version="1.0" encoding="UTF-8"?>' . PHP_EOL
+             . '<?xml-stylesheet type="text/xsl" href="/user/plugins/sitemap/sitemap.xsl"?>' . PHP_EOL
+             . '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' . PHP_EOL;
+
+        foreach ($routes as $name => $route) {
+            $lastMod = new \DateTime();
+            $lastModFormat = $lastMod->format('Y-m-d');
+
+            $xml .= '<url>' . PHP_EOL
+                . '<loc>' . $this->generateUrl($name, [], UrlGeneratorInterface::ABSOLUTE_URL) . '</loc>' . PHP_EOL
+                . '<lastmod>' . $lastModFormat . '</lastmod>' . PHP_EOL
+                . '<changefreq>' . $route->getDefaults()['changefreq'] . ' </changefreq>' . PHP_EOL
+                . '<priority>' . $route->getDefaults()['priority'] . '</priority>' . PHP_EOL
+                . '</url>' . PHP_EOL;
+        }
+
+        $xml .= '</urlset>';
+
+        $response = new Response($xml);
+        $response->headers->set('Content-Type', 'xml');
+
+        return $response;
     }
 }
