f57ca6ad3197fde4abcea679ee88fc5899ae0f7e
conradfr <yoyolebatteur@gmail.com>
2017-12-30 17:37:17 +0100

    Update Europe1 scraper due to page changes.

--- Scraper/radio_modules/europe1.js
+++ Scraper/radio_modules/europe1.js
@@ -26,76 +26,6 @@ const format = dateObj => {
             endDateTime.second(0);
         }
 
-        /*
-            Quikfix due to page changes
-
-            TODO: better regexp handling days and interval instead of IFs
-         */
-
-        // DU LUNDI AU JEUDI 18H00 - 19H00
-        if(match === null && dateObj.isoWeekday() < 5) {
-            regexp = new RegExp(/^DU LUNDI AU JEUDI ([0-9]{1,2})[h|H]([0-9]{2}) - ([0-9]{1,2})[h|H]([0-9]{2})/);
-            match = entry.datetime_raw.match(regexp);
-
-            if (match !== null) {
-                startDateTime.hour(match[1]);
-                startDateTime.minute(match[2]);
-                startDateTime.second(0);
-                endDateTime.hour(match[3]);
-                endDateTime.minute(match[4]);
-                endDateTime.second(0);
-            }
-        }
-
-        // LE VENDREDI DE 18H00 - 20H00
-        if(match === null && dateObj.isoWeekday() === 5) {
-            regexp = new RegExp(/^LE VENDREDI DE ([0-9]{1,2})[h|H]([0-9]{2}) - ([0-9]{1,2})[h|H]([0-9]{2})/);
-            match = entry.datetime_raw.match(regexp);
-
-            if (match !== null) {
-                startDateTime.hour(match[1]);
-                startDateTime.minute(match[2]);
-                startDateTime.second(0);
-                endDateTime.hour(match[3]);
-                endDateTime.minute(match[4]);
-                endDateTime.second(0);
-            }
-        }
-
-        // VENDREDI, SAMEDI ET DIMANCHE 20H00 - 23H00
-        if(match === null && dateObj.isoWeekday() > 4) {
-            regexp = new RegExp(/^VENDREDI, SAMEDI ET DIMANCHE ([0-9]{1,2})[h|H]([0-9]{2}) - ([0-9]{1,2})[h|H]([0-9]{2})/);
-            match = entry.datetime_raw.match(regexp);
-
-            if (match !== null) {
-                startDateTime.hour(match[1]);
-                startDateTime.minute(match[2]);
-                startDateTime.second(0);
-                endDateTime.hour(match[3]);
-                endDateTime.minute(match[4]);
-                endDateTime.second(0);
-            }
-        }
-
-        // 22H30-01H00 DU LUNDI AU JEUDI - 23H00-01H00 LE VENDREDI
-        if(match === null) {
-            regexp = new RegExp(/^([0-9]{1,2})[h|H]([0-9]{2})-([0-9]{1,2})[h|H]([0-9]{2}) DU LUNDI AU JEUDI - ([0-9]{1,2})[h|H]([0-9]{2})-([0-9]{1,2})[h|H]([0-9]{2}) LE VENDREDI/);
-            match = entry.datetime_raw.match(regexp);
-
-            if (match !== null) {
-                const index = (dateObj.isoWeekday() === 5) ? 5 : 1;
-
-                startDateTime.hour(match[index]);
-                startDateTime.minute(match[index+1]);
-                startDateTime.second(0);
-                endDateTime.hour(match[index+2]);
-                endDateTime.minute(match[index+3]);
-                endDateTime.second(0);
-            } else {
-                return prev;
-            }
-        }
-
         if (startDateTime.hour() > endDateTime.hour()) {
             endDateTime.add(1, 'days');
         }
@@ -105,12 +35,11 @@ const format = dateObj => {
         entry.schedule_end = endDateTime.toISOString();
         entry.timezone = 'Europe/Paris';
 
-        // not sure why I got an object that looks like an array here but well ...
-        if (typeof entry.description === 'object') {
-            entry.description = entry.description[0];
-        }
+        const host = entry.host.split('\n')[0];
+        entry.host = host;
 
         prev.push(entry);
+        console.log(entry);
         return prev;
     },[]);
 
@@ -119,14 +48,10 @@ const format = dateObj => {
 
 const fetch = dateObj => {
     const url = 'http://www.europe1.fr/grille-des-programmes';
+    // const url = 'http://progradio.local/europe1.html';
 
-    const dayOfWeek = dateObj.isoWeekday();
-    let tab = 'week';
-    if (dayOfWeek === 6) {
-        tab = 'saturday';
-    } else if (dayOfWeek === 7) {
-        tab = 'sunday';
-    }
+    dateObj.locale('fr');
+    const tab = dateObj.format('dddd').toLowerCase();
 
     logger.log('info', `fetching ${url}`);
 
@@ -134,18 +59,17 @@ const fetch = dateObj => {
         return osmosis
             .get(url)
             .find(`#${tab}Tab > .programmes`)
-            // .select('.wrap-img')
             .set({
-                'img': '.bloc.clearfix .wrap-img img@src'
+                'img': '.bloc .wrap-img img@src'
             })
             .set({
-                'host': '.bloc.clearfix .bloc_texte .titre',
-                'title': '.bloc.clearfix .bloc_texte span.heure > span',
-                'datetime_raw': '.bloc.clearfix .bloc_texte span.heure'
+                'host': '.bloc .bloc_texte .titre',
+                'title': '.bloc .bloc_texte .titre > span > a',
+                'datetime_raw': '.bloc .bloc_texte span.heure'
             })
-            .select('.bloc_programmes > li[1]')
+            // .select('.bloc_programmes > li[1]')
             .do(
-                osmosis.follow('a@href')
+                osmosis.follow('.bloc .bloc_texte .titre > span > a@href')
                     .find('div[itemprop=articleBody]')
                     .set({
                         'description':  'p[1]'
