d52f011dc10ddcec1a435019a8bcc633dd99f2ba
Lionel Cocatrix <yoyolebatteur@gmail.com>
2019-02-05 01:13:07 +0100

    [Scraper] Experimental scraper script to manage future collections and better handle concurrent scrapping  (wip, to be tested on prod env)

---
+++ Scraper/config/radios.js
@@ -0,0 +1,54 @@
+const radios = {
+    'nationwide':
+      [
+        // 'rtl',
+        'franceinter',
+        'rmc',
+        'nrj',
+        'europe1',
+        'franceinfo',
+        'nostalgie',
+        'funradio',
+        'rfm',
+        'skyrock',
+        'rtl2',
+        'virgin',
+        // 'franceculture',
+        'radioclassique',
+        'rireetchansons',
+        'radionova',
+        'sudradio',
+        'ouifm',
+        'cherie',
+        'mradio',
+        'mouv'
+      ],
+    'francebleu':
+      [
+        'rtl'
+      ]
+};
+
+const radiosModule = {
+    radios,
+    getCollections() {
+        return Object.keys(radios);
+    },
+    getRadios(collection) {
+        return radios[collection];
+    },
+    getRadiosWithPath(collection) {
+        return radios[collection];
+    },
+    getRadioPath(radio) {
+        for (let collection in radios) {
+            if (radios[collection].indexOf(radio) !== -1) {
+                return `${collection}/${radio}`;
+            }
+        }
+
+        return null;
+    }
+};
+
+module.exports = radiosModule;
