744eec3b41968f45a2c639aca703428f11d86527
Lionel Cocatrix <yoyolebatteur@gmail.com>
2020-09-15 18:26:24 +0200

    - Add polyfills for IE11 due to Moment->Lukon - Make vue-router works for IE11

--- public/vue/components/App.vue
+++ public/vue/components/App.vue
@@ -22,6 +22,17 @@ export default {
       navigator.mediaSession.setActionHandler('play', this.keyPlayPause.bind(this));
       navigator.mediaSession.setActionHandler('pause', this.keyPlayPause.bind(this));
     }
+
+    // making the router works on IE 11 https://github.com/vuejs/vue-router/issues/1849
+    if ('-ms-scroll-limit' in document.documentElement.style
+        && '-ms-ime-align' in document.documentElement.style) { // detect it's IE11
+      window.addEventListener('hashchange', () => {
+        const currentPath = window.location.hash.slice(1);
+        if (this.$route.path !== currentPath) {
+          this.$router.push(currentPath);
+        }
+      }, false);
+    }
   },
   methods: {
     /* eslint-disable func-names */
