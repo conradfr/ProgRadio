2de5c2aecc3efb3f19473e5f4e8e0c75efd0e740
Lionel Cocatrix <yoyolebatteur@gmail.com>
2021-02-17 10:27:14 +0100

    Add migration to new deployment.

--- tools/docker/www/docker-entry.sh
+++ tools/docker/www/docker-entry.sh
@@ -5,5 +5,7 @@ export RELEASE_ID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 
 cd /var/www && php bin/console cache:clear
 chmod -R 777 /var/www/var/cache
 
+cd /var/www && php bin/console d:m:m --no-interaction
+
 php-fpm &
 nginx -g "daemon off;"
