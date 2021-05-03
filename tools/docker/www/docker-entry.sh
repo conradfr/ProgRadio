a69f087360ae3e7e18f35f56bf1be73e6fdabfda
conradfr <yoyolebatteur@gmail.com>
2021-05-03 13:20:46 +0200

    Tools

--- tools/docker/www/docker-entry.sh
+++ tools/docker/www/docker-entry.sh
@@ -2,10 +2,10 @@
 
 export RELEASE_ID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
 
-#cd /var/www && php bin/console d:m:m --no-interaction --env=prod
+cd /var/www && php bin/console d:m:m --no-interaction --env=prod
 
 # will have to do better than that
-# chmod -R 777 /var/www/var
+chmod -R 777 /var/www/var
 
 php-fpm &
 nginx -g "daemon off;"
