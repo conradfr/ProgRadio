c57b3e36ef9b28cf2f6158ad887300923233d38d
conradfr <yoyolebatteur@gmail.com>
2021-07-15 12:54:06 +0200

    WIP of Vue in Admin / Listening section.

--- src/Repository/ListeningSessionRepository.php
+++ src/Repository/ListeningSessionRepository.php
@@ -110,28 +110,44 @@ class ListeningSessionRepository extends ServiceEntityRepository
         $baseResults = [
             'seo' => [
                 'total_radios' => 0,
-                'total_streams' => 0
+                'total_streams' => 0,
+                'list_radios' => [],
+                'list_streams' => []
             ],
             'web' => [
                 'total_radios' => 0,
-                'total_streams' => 0
+                'total_streams' => 0,
+                'list_radios' => [],
+                'list_streams' => []
             ]
         ];
 
         $sql = "
-            SELECT COALESCE(SUM(CASE WHEN ls.radio_stream_id is not null THEN 1 ELSE 0 END), 0) as total_radios,
-                   COALESCE(SUM(CASE WHEN ls.stream_id is not null THEN 1 ELSE 0 END), 0) as total_streams,
-                   ls.source                   
-            FROM listening_session ls
-            WHERE ls.date_time_end > (now() at time zone 'utc' - interval '32 second') and ls.source IN (:source1, :source2)
+            select ls.source,
+                    COALESCE(SUM(CASE WHEN ls.code_name is not null THEN 1 ELSE 0 END), 0) as total_radios,
+                    COALESCE(SUM(CASE WHEN ls.stream_id is not null THEN 1 ELSE 0 END), 0) as total_streams,
+                    COALESCE(json_agg(json_build_object('radio', ls.code_name, 'total', ls.total_radios))
+                        FILTER (WHERE ls.code_name IS NOT NULL), '[]') as list_radios,
+                    COALESCE(json_agg(json_build_object('stream', ls.stream_id, 'total', ls.total_streams))
+                        FILTER (WHERE ls.stream_id IS NOT NULL), '[]') as list_streams
+            FROM (
+                    select ls.source, r.code_name, count(r.id) as total_radios, s.id as stream_id, count(s.id) as total_streams
+                    from listening_session ls
+                    left join radio_stream rs on ls.radio_stream_id = rs.id
+                    left join radio r on rs.radio_id = r.id
+                    left join stream s on ls.stream_id = s.id
+                     WHERE ls.date_time_end > (now() at time zone 'utc' - interval '31 second')
+                       AND ls.source IN (:source1, :source2)
+                     GROUP BY ls.source, r.code_name, s.id
+            ) ls
             GROUP BY ls.source
-            /*GROUP BY ls.ip_address, ls.date_time_end
-            ORDER BY ls.date_time_end DESC*/;
         ";
 
         $rsm = new ResultSetMapping();
         $rsm->addScalarResult('total_radios', 'total_radios', 'integer')
             ->addScalarResult('total_streams', 'total_streams', 'integer')
+            ->addScalarResult('list_radios', 'list_radios', 'string')
+            ->addScalarResult('list_streams', 'list_streams', 'string')
             ->addScalarResult('source', 'source', 'string');
 
         $query = $this->getEntityManager()->createNativeQuery($sql, $rsm);
@@ -142,7 +158,15 @@ class ListeningSessionRepository extends ServiceEntityRepository
         $query->disableResultCache();
 
         $result = $query->getResult();
+        $resultIndexed = array_merge($baseResults, array_column($result, null, 'source'));
 
-        return array_merge($baseResults, array_column($result, null, 'source'));
+        foreach ($resultIndexed as &$row) {
+            $jsonRadios = is_array($row['list_radios']) ? $row['list_radios'] : json_decode($row['list_radios']);
+            $jsonStreams = is_array($row['list_streams']) ? $row['list_streams'] : json_decode($row['list_streams']);
+            $row['list_radios'] =  array_column($jsonRadios, 'total', 'radio');
+            $row['list_streams'] = array_column($jsonStreams, 'total', 'stream');
+        }
+
+        return $resultIndexed;
     }
 }
