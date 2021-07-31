98d14e2589bb10569403b4b9c15a0c9b95f7b244
conradfr <yoyolebatteur@gmail.com>
2021-07-31 14:34:55 +0200

    [Scraper][Api] Update L'Essentiel Radio

--- Scraper/radio_modules/international/lessentielradio.js
+++ Scraper/radio_modules/international/lessentielradio.js
@@ -24,28 +24,66 @@ const format = dateObj => {
       return prev;
     }
 
+    let time = [];
+
     let regexp = new RegExp(/^De\s(Lundi|Mardi|Mercredi|Jeudi|Vendredi|Samedi|Dimanche)\sà\s(Lundi|Mardi|Mercredi|Jeudi|Vendredi|Samedi|Dimanche), de ([0-9]{1,2})[:]([0-9]{2})\sà\s([0-9]{1,2})[:]([0-9]{2})/);
     let match = entry.datetime_raw.match(regexp);
 
     if (match === null) {
-      return prev;
+      // maybe it's weekend
+      regexp = new RegExp(/^Le week-end, de ([0-9]{1,2})[:]([0-9]{2})\sà\s([0-9]{1,2})[:]([0-9]{2})/);
+      match = entry.datetime_raw.match(regexp);
+
+      if (match !== null) {
+        // not week-end ?
+        const dayNum = dateObj.isoWeekday();
+        if (dayNum !== 6 && dayNum !== 7) {
+          return prev;
+        }
+
+        time = [match[1], match[2], match[3], match[4]];
+      }
+    } else {
+      // not in day interval
+      const dayNum = dateObj.isoWeekday();
+      if (dayNum < dayFr[match[1]] || dayNum > dayFr[match[2]]) {
+        return prev;
+      }
+
+      time = [match[3], match[4], match[5], match[6]];
     }
 
-    // not in day interval
-    const dayNum = dateObj.isoWeekday();
-    if (dayNum < dayFr[match[1]] || dayNum > dayFr[match[2]]) {
-      return prev;
+    // maybe it's just one day
+    if (match === null) {
+      regexp = new RegExp(/^(Lundi|Mardi|Mercredi|Jeudi|Vendredi|Samedi|Dimanche), de ([0-9]{1,2})[:]([0-9]{2})\sà\s([0-9]{1,2})[:]([0-9]{2})/);
+      match = entry.datetime_raw.match(regexp);
+
+      if (match === null) {
+        return prev;
+      }
+
+      const dayNum = dateObj.isoWeekday();
+      if (dayNum !==  dayFr[match[1]]) {
+        return prev;
+      }
+
+      time = [match[2], match[3], match[4], match[5]];
     }
 
     let startDateTime = moment(dateObj);
-    startDateTime.hour(match[3]);
-    startDateTime.minute(match[4]);
+    startDateTime.hour(time[0]);
+    startDateTime.minute(time[1]);
     startDateTime.second(0);
 
     endDateTime = moment(dateObj);
-    endDateTime.hour(match[5]);
-    endDateTime.minute(match[6]);
+    endDateTime.hour(time[2]);
+    endDateTime.minute(time[3]);
     endDateTime.second(0);
+
+    // end at midnight etc
+    if (time[2] < time[0]) {
+      endDateTime.add(1, 'days');
+    }
 
     const newEntry = {
       'date_time_start': startDateTime.toISOString(),
---
+++ progradio_api/lib/progradio_api/song_provider/lessentielradio.ex
@@ -0,0 +1,25 @@
+defmodule ProgRadioApi.SongProvider.Lessentielradio do
+  require Logger
+  alias ProgRadioApi.SongProvider.GenericLesIndes
+
+  @behaviour ProgRadioApi.SongProvider
+
+  @url "http://lessentielradio.lu/cache/titreplayer1"
+
+  @impl true
+  defdelegate has_custom_refresh(), to: GenericLesIndes
+
+  @impl true
+  defdelegate get_refresh(name, data, default_refresh), to: GenericLesIndes
+
+  @impl true
+  defdelegate get_auto_refresh(name, data, default_refresh), to: GenericLesIndes
+
+  @impl true
+  def get_data(name, last_data) do
+    GenericLesIndes.get_data(@url, name, last_data)
+  end
+
+  @impl true
+  defdelegate get_song(name, data), to: GenericLesIndes
+end
