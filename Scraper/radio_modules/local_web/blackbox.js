c57566b3ff0699a1c3ffe1ec16eb64ceb0becc6f
conradfr <yoyolebatteur@gmail.com>
2021-08-02 18:13:14 +0200

    [Scraper][Api] Update Blackbox

--- Scraper/radio_modules/local_web/blackbox.js
+++ Scraper/radio_modules/local_web/blackbox.js
@@ -1,12 +1,13 @@
-const scrapAbstract = require('../regional/_abstract_lesindes.js');
+const scrapAbstract = require('../regional/_abstract_lesindes2.js');
 
 const name = 'blackbox';
 
 const getScrap = dateObj => {
-  const url = 'https://www.blackboxfm.fr/programs';
-  const img_prefix = 'https://www.blackboxfm.fr';
-  return scrapAbstract.getScrap(dateObj, url, name, img_prefix)
+  const url = 'https://www.blackboxfm.fr/emissions';
+  const description_prefix = 'https://www.blackboxfm.fr';
+  return scrapAbstract.getScrap(dateObj, url, name, description_prefix);
 };
+
 
 const scrapModule = {
   getName: name,
--- progradio_api/lib/progradio_api/song_provider/blackbox.ex
+++ progradio_api/lib/progradio_api/song_provider/blackbox.ex
@@ -1,25 +1,26 @@
 defmodule ProgRadioApi.SongProvider.Blackbox do
   require Logger
-  alias ProgRadioApi.SongProvider.GenericLesIndes
+  alias ProgRadioApi.SongProvider.GenericLesIndes2
 
   @behaviour ProgRadioApi.SongProvider
 
-  @url "https://www.blackboxfm.fr/players/index/gettitrageplayer/idplayers/2174546520932614196"
+  @url "https://api.latina.fr/graphql"
+  @radio_id "2174546520932614196"
 
   @impl true
-  defdelegate has_custom_refresh(), to: GenericLesIndes
+  defdelegate has_custom_refresh(), to: GenericLesIndes2
 
   @impl true
-  defdelegate get_refresh(name, data, default_refresh), to: GenericLesIndes
+  defdelegate get_refresh(name, data, default_refresh), to: GenericLesIndes2
 
   @impl true
-  defdelegate get_auto_refresh(name, data, default_refresh), to: GenericLesIndes
+  defdelegate get_auto_refresh(name, data, default_refresh), to: GenericLesIndes2
 
   @impl true
-  def get_data(name, last_data) do
-    GenericLesIndes.get_data(@url, name, last_data)
+  def get_data(name, _last_data) do
+    GenericLesIndes2.get_data(@url, name, @radio_id)
   end
 
   @impl true
-  defdelegate get_song(name, data), to: GenericLesIndes
+  defdelegate get_song(name, data), to: GenericLesIndes2
 end
