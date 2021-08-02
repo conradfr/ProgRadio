e2762828f727094cc206a399e03bbcf5e240b41c
conradfr <yoyolebatteur@gmail.com>
2021-08-02 17:05:41 +0200

    [Scraper][Api] Update Latina

--- Scraper/radio_modules/regional/latina.js
+++ Scraper/radio_modules/regional/latina.js
@@ -1,11 +1,11 @@
-const scrapAbstract = require('./_abstract_lesindes.js');
+const scrapAbstract = require('./_abstract_lesindes2.js');
 
 const name = 'latina';
 
 const getScrap = dateObj => {
-  const url = 'https://www.latina.fr/programs';
-  const img_prefix = 'https://www.latina.fr';
-  return scrapAbstract.getScrap(dateObj, url, name, img_prefix)
+  const url = 'https://www.latina.fr/emissions';
+  const description_prefix = 'https://www.latina.fr';
+  return scrapAbstract.getScrap(dateObj, url, name, description_prefix);
 };
 
 const scrapModule = {
