e9a3c735473e5946f64d47e1ee39131e6c9e863d
conradfr <yoyolebatteur@gmail.com>
2021-08-10 19:04:08 +0200

    [SCraper] Update Forum (weird url change)

--- Scraper/radio_modules/regional/forum.js
+++ Scraper/radio_modules/regional/forum.js
@@ -3,7 +3,7 @@ const scrapAbstract = require('./_abstract_lesindes2.js');
 const name = 'forum';
 
 const getScrap = dateObj => {
-  const url = 'https://www.forum.fr/emissions';
+  const url = 'https://www.forum.fr/emissions/emissions-2';
   const description_prefix = 'https://www.forum.fr';
   return scrapAbstract.getScrap(dateObj, url, name, description_prefix);
 };
