b6dca5db2002243ef315e2f7def5080a5c527e39
conradfr <yoyolebatteur@gmail.com>
2021-08-29 13:04:19 +0200

    [Scraper] Update Africa Radio

--- Scraper/radio_modules/regional/africaradio.js
+++ Scraper/radio_modules/regional/africaradio.js
@@ -1,12 +1,12 @@
-const scrapAbstract = require('./_abstract_lesindes.js');
+const scrapAbstract = require('./_abstract_lesindes2.js');
 
 const name = 'africaradio';
 
 const getScrap = dateObj => {
   dateObj.tz('GMT');
   const url = 'https://www.africaradio.com/emissions';
-  const img_prefix = 'https://www.africaradio.com';
-  return scrapAbstract.getScrap(dateObj, url, name, img_prefix)
+  const description_prefix = 'https://www.africaradio.com';
+  return scrapAbstract.getScrap(dateObj, url, name, description_prefix)
 };
 
 const scrapModule = {
