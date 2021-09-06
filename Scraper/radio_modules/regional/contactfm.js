7b6b967429c38f67e237da1c986bf9c86393b362
conradfr <yoyolebatteur@gmail.com>
2021-09-06 18:28:09 +0200

    [Front][Api] Update ContactFM

--- Scraper/radio_modules/regional/contactfm.js
+++ Scraper/radio_modules/regional/contactfm.js
@@ -1,11 +1,11 @@
-const scrapAbstract = require('./_abstract_lesindes.js');
+const scrapAbstract = require('./_abstract_lesindes2.js');
 
 const name = 'contactfm';
 
 const getScrap = dateObj => {
   const url = 'https://www.mycontact.fr/emissions';
-  const img_prefix = 'https://www.mycontact.fr';
-  return scrapAbstract.getScrap(dateObj, url, name, img_prefix)
+  const description_prefix = 'https://www.mycontact.fr';
+  return scrapAbstract.getScrap(dateObj, url, name, description_prefix);
 };
 
 const scrapModule = {
