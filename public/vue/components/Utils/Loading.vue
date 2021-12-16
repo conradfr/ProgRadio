d51df09bc616549070bfa49b0b5fefe93d2d36f5
conradfr <yoyolebatteur@gmail.com>
2021-12-16 12:55:02 +0100

    [Front] Replace loading icon w/ the Bootstrap spinner

--- public/vue/components/Utils/Loading.vue
+++ public/vue/components/Utils/Loading.vue
@@ -2,9 +2,7 @@
   <transition appear>
     <div class="loading" v-if="isLoading">
       <div class="d-flex align-items-center alert alert-progradio" role="alert">
-        <div class="alert-progradio-icon refresh-animate">
-          <i class="bi bi-arrow-repeat"></i>
-        </div>
+        <div class="spinner-border" role="status"></div>
         <div class="flex-grow-1 alert-progradio-text text-center" v-once>
           {{ $t('message.loading') }}
         </div>
