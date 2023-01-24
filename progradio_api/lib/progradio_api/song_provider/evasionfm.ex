67d5e3cb6793ad5ac5144cf6f356f8892e8acab3
conradfr <yoyolebatteur@gmail.com>
2023-01-24 20:48:58 +0100

    [api][scraper] Update Evasion radio

--- progradio_api/lib/progradio_api/song_provider/evasionfm.ex
+++ progradio_api/lib/progradio_api/song_provider/evasionfm.ex
@@ -1,56 +1,23 @@
 defmodule ProgRadioApi.SongProvider.Evasionfm do
   require Logger
-  alias ProgRadioApi.SongProvider
+  alias ProgRadioApi.SongProvider.GenericLesIndes2
 
   @behaviour ProgRadioApi.SongProvider
 
-  @url "https://www.evasionfm.com/XML/morceaux.xml"
-  @minutes_max 300
-  @timezone "Europe/Paris"
+  @url "https://api.evasionfm.com/graphql"
+  @radio_id "2174546520932614292"
 
   @impl true
-  def has_custom_refresh(), do: false
+  defdelegate has_custom_refresh(), to: GenericLesIndes2
 
   @impl true
-  def get_refresh(_name, _data, _default_refresh), do: nil
+  defdelegate get_refresh(name, data, default_refresh), to: GenericLesIndes2
 
   @impl true
-  def get_data(_name, _last_data) do
-    now_unix = SongProvider.now_unix()
-
-    @url
-    |> SongProvider.get()
-    |> Map.get(:body)
-    |> XmlToMap.naive_map()
-    |> Map.get("listeMorceaux", %{})
-    |> Map.get("morceau", [])
-    |> Enum.find(nil, fn e ->
-      try do
-        time_start =
-          e
-          |> Map.get("heure")
-          |> Timex.parse!("{h24}h{m}")
-          |> NaiveDateTime.to_time()
-          |> (&DateTime.new!(DateTime.now!(@timezone) |> DateTime.to_date(), &1, @timezone)).()
-          |> DateTime.to_unix()
-
-        time_end = time_start + @minutes_max
-        now_unix >= time_start and now_unix <= time_end
-      rescue
-        _ -> nil
-      end
-    end)
+  def get_data(name, _last_data) do
+    GenericLesIndes2.get_data(@url, name, @radio_id)
   end
 
   @impl true
-  def get_song(name, data) do
-    case data do
-      nil ->
-        Logger.info("Data provider - #{name}: error fetching song data or empty")
-        %{}
-
-      _ ->
-        %{artist: SongProvider.recase(data["artiste"]), title: SongProvider.recase(data["titre"])}
-    end
-  end
+  defdelegate get_song(name, data), to: GenericLesIndes2
 end
