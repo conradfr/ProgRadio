3eae2cf3049bbd3b040b5cf012d0bc5c73ec28d2
conradfr <yoyolebatteur@gmail.com>
2024-03-02 19:35:24 +0100

    [www][spa] Remove obsolete & dead AdSense code.

--- public/vue/components/Streams/StreamsListPagination.vue
+++ public/vue/components/Streams/StreamsListPagination.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <div class="row">
-      <div class="col-md-12 pb-3 pb-md-0">
+      <div class="col-md-12 pb-3 pb-md-2">
         <nav aria-label="navigation">
           <ul class="pagination justify-content-center">
             <li class="page-item" :class="{ 'disabled': page === 1 }">
