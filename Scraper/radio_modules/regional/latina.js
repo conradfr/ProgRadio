298af2ef8931f1d6e95afb0f94f80093bb2183cd
conradfr <yoyolebatteur@gmail.com>
2024-05-08 07:31:33 +0200

    [scraper] Update some Les Indés radios.

--- Scraper/radio_modules/regional/latina.js
+++ Scraper/radio_modules/regional/latina.js
@@ -3,7 +3,7 @@ const scrapAbstract = require('./_abstract_lesindes2.js');
 const name = 'latina';
 
 const getScrap = dateObj => {
-  const url = 'https://www.latina.fr/emissions';
+  const url = 'https://www.latina.fr/emissions/1';
   const description_prefix = 'https://www.latina.fr';
   return scrapAbstract.getScrap(dateObj, url, name, description_prefix);
 };
