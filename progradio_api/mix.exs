ea51458196deac551a5d0ad029c20830d1f0db97
conradfr <yoyolebatteur@gmail.com>
2024-08-13 21:43:38 +0200

    [api] Update to Elixir 1.17 / OTP 27

--- progradio_api/mix.exs
+++ progradio_api/mix.exs
@@ -4,8 +4,8 @@ defmodule ProgRadioApi.MixProject do
   def project do
     [
       app: :progradio_api,
-      version: "0.4.0",
-      elixir: "~> 1.15",
+      version: "0.5.0",
+      elixir: "~> 1.17",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
