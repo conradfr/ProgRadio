ddb8da745e42ba6b8d107284069971e91301c5ed
lcocatrix <contact@gearpix.app>
2024-11-23 22:53:42 +0100

    [api] Update Africa radio live song

--- progradio_api/lib/progradio_api/song_provider/africaradio.ex
+++ progradio_api/lib/progradio_api/song_provider/africaradio.ex
@@ -1,25 +1,69 @@
 defmodule ProgRadioApi.SongProvider.Africaradio do
   require Logger
-  alias ProgRadioApi.SongProvider.GenericLesIndes3
+  alias ProgRadioApi.SongProvider
 
   @behaviour ProgRadioApi.SongProvider
 
-  #  @url "https://api.africaradio.com/graphql"
-  @url "https://www.africaradio.com/api/TitleDiffusions"
+  @stream_ids %{
+    "africaradio_main" => "principal",
+    "africaradio_abidjan" => "abidjan",
+    "africaradio_club" => "club",
+    "africaradio_coupe_dk" => "coupe_dk",
+    "africaradio_mandingue" => "mandingue",
+    "africaradio_naija" => "naija",
+    "africaradio_manu-dibango-forever" => "manu-dibango-forever",
+    "africaradio_rumba" => "rumba",
+    "africaradio_selecta" => "selecta",
+  }
 
-  @radio_id "2180540611545792571"
+  @refresh_fallback_s 3
+  @refresh_fallback_ms 3000
 
   @impl true
-  defdelegate has_custom_refresh(), to: GenericLesIndes3
+  def has_custom_refresh(), do: true
 
   @impl true
-  defdelegate get_refresh(name, data, default_refresh), to: GenericLesIndes3
+  def get_refresh(_name, nil, default_refresh), do: default_refresh
 
   @impl true
-  def get_data(name, _last_data) do
-    GenericLesIndes3.get_data(@url, name, @radio_id)
+  def get_refresh(_name, data, default_refresh) do
+    next = Map.get(data, "delayToRefresh", default_refresh) + 5
+
+    if next < @refresh_fallback_s do
+      @refresh_fallback_ms
+    else
+      next
+    end
   end
 
   @impl true
-  defdelegate get_song(name, data), to: GenericLesIndes3
+  def get_data(name, _last_data) do
+    id =
+      SongProvider.get_stream_code_name_from_channel(name)
+      |> (&Map.get(@stream_ids, &1)).()
+
+    url = "https://www.africaradio.com/ajax/refresh-radio-title.php?radio=#{id}"
+
+    try do
+      url
+      |> SongProvider.get()
+      |> Map.get(:body)
+      |> Jason.decode!()
+      |> Map.get("now", nil)
+    rescue
+      _ -> nil
+    end
+  end
+
+  @impl true
+  def get_song(name, data) do
+    case data do
+      nil -> nil
+      _ ->
+        artist = Map.get(data, "artiste")
+        title = Map.get(data, "titre")
+
+        %{artist: artist, title: title}
+    end
+  end
 end
