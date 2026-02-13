b098cf8e44621b5906da2e2f0e31d150515bb80c
lcocatrix <lionel.cocatrix@gmail.com>
2026-02-13 11:16:08 +0100

    [api] Update Radio La Belle Aventure live song.

--- progradio_api/lib/progradio_api/song_provider/radiolabelleaventure.ex
+++ progradio_api/lib/progradio_api/song_provider/radiolabelleaventure.ex
@@ -1,29 +1,52 @@
 defmodule ProgRadioApi.SongProvider.Radiolabelleaventure do
   require Logger
+  alias ProgRadioApi.SongProvider
+
+  # this is adapted from azuracast
 
   @behaviour ProgRadioApi.SongProvider
 
-  @url "https://widgetsv2.mediacpanel.com/webplayerv2/common/interval.php?user=tyizqrhg&fingerprinting="
+  @url "https://flux.radiolabelleaventure.com/api/nowplaying/radio_la_belle_aventure"
 
   @impl true
-  def has_custom_refresh(_name), do: false
+  def has_custom_refresh(_name), do: true
 
   @impl true
-  def get_refresh(_name, _data, _default_refresh), do: nil
+  def get_refresh(name, data, default_refresh) do
+    try do
+      now_unix = SongProvider.now_unix()
+      end_at = Map.get(data, "played_at", 0) + Map.get(data, "duration", 0)
+
+      if end_at > now_unix do
+        next_refresh = end_at - now_unix
+        Logger.debug("Data provider - #{name} (radiolabelleaventure) - next refresh: #{next_refresh}")
+        next_refresh * 1000
+      else
+        default_refresh
+      end
+    rescue
+      _ -> default_refresh
+    end
+  end
 
   @impl true
   def get_data(name, _last_data) do
     try do
-      @url
-      |> Req.post!(
-        form: [nowPlaying: "1"],
-        headers: [{"Cache-Control", "no-cache"}],
-        redirect: false,
-        connect_options: [timeout: 15_000]
-      )
-      |> Map.get(:body)
-      |> :json.decode()
-      |> List.first()
+      data =
+        @url
+        |> SongProvider.get()
+        |> Map.get(:body)
+        |> :json.decode()
+        |> Map.get("now_playing", %{})
+
+      now_unix = SongProvider.now_unix()
+      end_at = Map.get(data, "played_at", 0) + Map.get(data, "duration", 0)
+
+      if end_at > now_unix or end_at == 0 do
+        data
+      else
+        nil
+      end
     rescue
       _ ->
         Logger.debug("Data provider - #{name} (radiolabelleaventure): data error rescue")
@@ -34,17 +57,30 @@ defmodule ProgRadioApi.SongProvider.Radiolabelleaventure do
   @impl true
   def get_song(name, data, _last_song) do
     try do
-      title =
-        case Map.get(data, "title") do
-          nil -> nil
-          value -> String.replace(value, " by <br>", "")
-        end
+      artist =
+        data
+        |> Map.get("song", %{})
+        |> Map.get("artist")
 
-      %{
-        artist: Map.get(data, "artist"),
-        title: title,
-        cover_url: Map.get(data, "artwork")
-      }
+      title =
+        data
+        |> Map.get("song", %{})
+        |> Map.get("title")
+
+      cover =
+        data
+        |> Map.get("song", %{})
+        |> Map.get("art")
+
+      if artist != nil or title != nil do
+        %{
+          artist: artist,
+          title: title,
+          cover_url: cover
+        }
+      else
+        nil
+      end
     rescue
       _ ->
         Logger.debug(
